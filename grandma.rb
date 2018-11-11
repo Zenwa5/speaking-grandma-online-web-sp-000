bb5887b18dbd7127ff1f9af50f06194daac6b4d7
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-11 02:10:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,7 @@ def speak_to_grandma(phrase = "I love you GRANDMA")
   elsif phrase == phrase.reverse
     return "NO, NOT SINCE 1938!"
   else
-    return ""
+    return "HUH?! SPEAK UP, SONNY!
   end
 end
 
